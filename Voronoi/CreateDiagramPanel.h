14ddbe5043558153a10a6d6a0b93695238e2fca0
Zack Brown <Zack.Brown@3squared.com>
2013-11-27 12:02:16 +0000

    Amended NSInteger properties to NSUInteger.

--- Voronoi/CreateDiagramPanel.h
+++ Voronoi/CreateDiagramPanel.h
@@ -21,9 +21,9 @@ typedef NS_ENUM(NSInteger, DiagramType)
 
 @protocol CreateDiagramPanelDelegate <NSObject>
 
-- (void)createDiagramPanelDidConfirmWithGridDiagramType:(CreateDiagramPanel *)panel xMargin:(NSInteger)xMargin yMargin:(NSInteger)yMargin numberOfIterations:(NSInteger)numberOfIterations columns:(NSInteger)columns rows:(NSInteger)rows;
-- (void)createDiagramPanelDidConfirmWithRandomDiagramType:(CreateDiagramPanel *)panel xMargin:(NSInteger)xMargin yMargin:(NSInteger)yMargin numberOfIterations:(NSInteger)numberOfIterations numberOfSites:(NSInteger)numberOfSites seed:(NSInteger)seed;
-- (void)createDiagramPanelDidConfirmWithSpiralDiagramType:(CreateDiagramPanel *)panel xMargin:(NSInteger)xMargin yMargin:(NSInteger)yMargin numberOfIterations:(NSInteger)numberOfIterations spiralChord:(CGFloat)spiralChord;
+- (void)createDiagramPanelDidConfirmWithGridDiagramType:(CreateDiagramPanel *)panel xMargin:(NSUInteger)xMargin yMargin:(NSUInteger)yMargin numberOfIterations:(NSUInteger)numberOfIterations columns:(NSUInteger)columns rows:(NSUInteger)rows;
+- (void)createDiagramPanelDidConfirmWithRandomDiagramType:(CreateDiagramPanel *)panel xMargin:(NSUInteger)xMargin yMargin:(NSUInteger)yMargin numberOfIterations:(NSUInteger)numberOfIterations numberOfSites:(NSUInteger)numberOfSites seed:(NSUInteger)seed;
+- (void)createDiagramPanelDidConfirmWithSpiralDiagramType:(CreateDiagramPanel *)panel xMargin:(NSUInteger)xMargin yMargin:(NSUInteger)yMargin numberOfIterations:(NSUInteger)numberOfIterations spiralChord:(CGFloat)spiralChord;
 - (void)createDiagramPanelDidCancel:(CreateDiagramPanel *)panel;
 
 @end
